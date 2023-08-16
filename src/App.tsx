dc8f85545edb221e19fbdfb4a76e7c26050b6fb0
uji <ujimoto.daichi@moneyforward.co.jp>
2023-08-16 18:19:52 +0900

    write my profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ujiIcon from './assets/uji.jpg'
+import xIcon from './assets/x.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Card() {
+  return (
+    <div className="card">
+      <a href="https://twitter.com/uji_rb" target="_blank">
+        <img src={xIcon} className="logo" alt="X logo" />
+      </a>
+      <a href="https://twitter.com/uji_rb" target="_blank">
+        <p>@uji_rb</p>
+      </a>
+    </div>
+  )
+}
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={ujiIcon} className="icon" alt="Uji icon" />
+      <h1>uji</h1>
+      <p>Software developer @MoneyForward</p>
+      <Card />
     </>
   )
 }
